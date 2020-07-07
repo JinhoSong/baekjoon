3df05d7fbe350ccf60e40c4ed1490e2a7cae8416
JinhoSong <chosunz4@naver.com>
2020-07-07 16:24:40 +0900

    Input Output Setting

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,21 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int p [] = new int [n];
-        int min = 1000000;
         int max = -1000000;
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int index = 0;
+        //StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            p[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(br.readLine());
         }
 
         for (int i = 0; i < n; i++) {
-            if(p[i] > max )
-                max= p[i];
-            if( p[i] < min)
-                min = p[i];
+            if(p[i] > max ) {
+                index = i;
+                max = p[i];
+            }
         }
-        bw.write(Integer.toString(min)+" "+Integer.toString(max));
+        bw.write(Integer.toString(max));
+        bw.write(Integer.toString(index));
         bw.flush();
         br.close();
         bw.close();
