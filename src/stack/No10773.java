1c9369222822324401effc1ab7d65a13a423af72
JinhoSong <chosunz4@naver.com>
2020-08-28 00:38:06 +0900

    가장 최근에 쓴 수를 지우는 문제

--- src/stack/No10773.java
+++ src/stack/No10773.java
@@ -1,6 +1,4 @@
 package stack;
-
-
 import java.io.*;
 import java.util.Stack;
 class baekjoon10773 {
@@ -9,7 +7,6 @@ class baekjoon10773 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < n; i++) {
             int str = Integer.parseInt(br.readLine());
             switch (str){
@@ -18,19 +15,13 @@ class baekjoon10773 {
             }
         }
         int result = 0;
-        if(stack.pop() == '(')// 검사할 필요도 없이 실패
+        if(stack.empty() == true)
             bw.write(0+"\n");
-        else { // '('로 시작 안했으면 완벽하게 괄호가 다 닫히면 양쪽 갯수가 똑같아진다.
+        else {
             while (!stack.isEmpty()) {
-                if( stack.pop() =='(')
-                    result++;
-                else
-                    result--;
+                result += stack.pop();
             }
-            if( result == 0)
-                bw.write("NO"+"\n");
-            else
-                bw.write("YES"+"\n");
+            bw.write(result+"\n");
         }
         bw.flush();
         bw.close();
@@ -41,4 +32,3 @@ public class No10773 {
         new baekjoon10773().No10773();
     }
 }
-
