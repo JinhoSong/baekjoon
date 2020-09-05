dda578c2c00625625c28f7b55f7d41f2d9784c57
JinhoSong <chosunz4@naver.com>
2020-09-05 15:59:27 +0900

    문자 아스키코드 반환

---
+++ src/string/No11654.java
@@ -0,0 +1,22 @@
+package string;
+
+import java.io.*;
+
+class baekjoon {
+    public void No11654() throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char word = br.readLine().charAt(0);
+        int number = (int)word;
+        bw.write(number+"");
+        bw.flush();
+        bw.close();
+
+    }
+}
+
+public class No11654 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        new baekjoon().No11654();
+    }
+}
