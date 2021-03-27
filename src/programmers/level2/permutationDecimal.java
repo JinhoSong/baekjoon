a6a58846f0a9a67b288ed4da3ecb04c69ad0a0b8
JinhoSong <chosunz4@naver.com>
2021-03-27 18:39:46 +0900

    2020 03 27

--- src/programmers/level2/permutationDecimal.java
+++ src/programmers/level2/permutationDecimal.java
@@ -9,9 +9,12 @@ public class permutationDecimal {
         System.out.println((solution(numbers)));
     }
 
-    private static int count=0;
-    private static boolean[]decimal;
 
+    private static int count = 0;
+    private static boolean[] decimal;
+    private static int[] arr;
+    private static String[] str;
+    private static boolean[] visited;
     public static int solution(String numbers) {
         // 아리스토의 체를 만들어 두고
         // 7칸이 가능하니까 0이 7개
@@ -28,7 +31,10 @@ public class permutationDecimal {
         }
         // 소수를 모두 세팅해두고
 
-        permutation("",numbers);
+        dfs("",0,arr.length);
+
+        // permutation("",numbers);
+
         return count;
     }
     public static void permutation(String prefix, String str){
@@ -46,8 +52,31 @@ public class permutationDecimal {
             permutation(prefix+str.charAt(i),str.substring(0,i)+str.substring(i+1,n));
         }
     }
+
+    public static void dfs(String prefix, int depth,int len) {
+        if (depth == len) {
+            int num = Integer.parseInt(prefix);
+            if(!decimal[num]){
+                count++;
+                decimal[num] = true;
+            }
+            return;
+        }
+        while(!(len ==-1)){
+            for (int i = 0; i < arr.length; i++) {
+                if (!visited[i]) {
+                    //방문한 적이 없다면
+                    prefix = prefix + str[i];
+                    visited[i] = true;
+                    dfs(prefix, depth + 1,len);
+                    prefix = prefix.substring(0, prefix.length() - 1);
+                    visited[i] = false;
+                }
+            }
+            len-=1;
+        }
+    }
+
 }
-
-
 
 
